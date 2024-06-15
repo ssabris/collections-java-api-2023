153d7cf57820b2aeccb5f3b58ccd97958a319548
sabrina <sabrinasaabs@gmail.com>
2024-06-15 19:34:36 -0300

    Resolvendo exercicios set
    Signed-off-by: sabrina <sabrinasaabs@gmail.com>

--- src/main/java/set/OperacoesBasicas2/ConjuntoConvidados2.java
+++ src/main/java/set/OperacoesBasicas2/ConjuntoConvidados2.java
@@ -1,4 +1,55 @@
 package main.java.set.OperacoesBasicas2;
 
-public class ConjuntoConvidados {
+import java.util.HashSet;
+import java.util.Set;
+
+public class ConjuntoConvidados2 {
+    //atributo
+    private Set<Convidado2> convidado2Set;
+    public ConjuntoConvidados2() {
+        this.convidado2Set = new HashSet<>();
+    }
+
+    public void adicionarConvidado2(String nome, int codigoConvite) {
+        convidado2Set.add(new Convidado2(nome, codigoConvite));
+    }
+
+    public void removerConvidadoPorCodigoConvite(int codigoConvite){
+        Convidado2 convidadoParaRemover = null;
+        for (Convidado2 c : convidado2Set) {
+            if (c.getCodigoConvite() == codigoConvite) {
+                convidadoParaRemover = c;
+                break;
+            }
+        }
+
+        convidado2Set.remove(convidadoParaRemover);
+    }
+
+    public int contarConvidados() {
+        return convidado2Set.size();
+    }
+
+    public void exibirConvidados(){
+        System.out.println(convidado2Set);
+    }
+
+    public static void main(String[] args) {
+        ConjuntoConvidados2 conjuntoConvidados2 = new ConjuntoConvidados2();
+        System.out.println("Existem " + conjuntoConvidados2.contarConvidados() + " convidados dentro do Set de Convidados");
+
+        conjuntoConvidados2.adicionarConvidado2("Convidado 1", 1234);
+        conjuntoConvidados2.adicionarConvidado2("Convidado 2", 1235);
+        conjuntoConvidados2.adicionarConvidado2("Convidado 3", 1235);
+        conjuntoConvidados2.adicionarConvidado2("Convidado 4", 1236);
+
+        System.out.println("Existem " + conjuntoConvidados2.contarConvidados() + " convidados dentro do Set de Convidados");
+
+        conjuntoConvidados2.removerConvidadoPorCodigoConvite(1236);
+        System.out.println("Existem " + conjuntoConvidados2.contarConvidados() + " convidados dentro do Set de Convidados");
+
+        conjuntoConvidados2.exibirConvidados();
+
+
+    }
 }
--- src/main/java/set/OperacoesBasicas2/Convidado2.java
+++ src/main/java/set/OperacoesBasicas2/Convidado2.java
@@ -1,2 +1,43 @@
-package main.java.set.OperacoesBasicas2;public class Convidado2 {
+package main.java.set.OperacoesBasicas2;
+
+import java.util.Objects;
+
+public class Convidado2 {
+    //atributos
+    private String nome;
+    private int codigoConvite;
+
+    public Convidado2(String nome, int codigoConvite) {
+        this.nome = nome;
+        this.codigoConvite = codigoConvite;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getCodigoConvite() {
+        return codigoConvite;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Convidado2 that = (Convidado2) o;
+        return getCodigoConvite() == that.getCodigoConvite();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getCodigoConvite());
+    }
+
+    @Override
+    public String toString() {
+        return "Convidado2{" +
+                "nome='" + nome + '\'' +
+                ", codigoConvite=" + codigoConvite +
+                '}';
+    }
 }
